d934c26bf51c991fe14d619bb39f6a814196feb3
Gabriel Wu <qqbbnease1004@126.com>
2020-01-12 00:04:36 +0800

    templates: init

---
+++ templates/template.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+class Solution {
+ public:
+  void solve() {
+    int n;
+    cin >> n;
+  }
+};
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    Solution solution = Solution();
+    solution.solve();
+  }
+}
