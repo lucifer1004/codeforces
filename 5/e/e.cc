13d46fcc9cc7f20f0a4630a76bbb63c1ba822499
Gabriel Wu <wuzihua@pku.edu.cn>
2020-01-13 15:21:13 +0800

    5: reformat E

--- 5/e/e.cc
+++ 5/e/e.cc
@@ -1,15 +1,5 @@
 #include <algorithm>
-#include <bitset>
-#include <climits>
-#include <cmath>
-#include <cstring>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
