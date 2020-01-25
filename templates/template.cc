95c849987d77dc9cea1ca204cb989f771c558c24
Gabriel Wu <qqbbnease1004@126.com>
2020-01-25 12:56:15 +0800

    template: update CC

--- templates/template.cc
+++ templates/template.cc
@@ -2,10 +2,17 @@
 #include <bitset>
 #include <climits>
 #include <cmath>
+#include <complex>
+#include <cstdio>
 #include <cstring>
+#include <ctime>
+#include <functional>
 #include <iostream>
+#include <iterator>
 #include <map>
+#include <numeric>
 #include <queue>
+#include <random>
 #include <set>
 #include <stack>
 #include <unordered_map>
@@ -15,17 +22,22 @@
 using namespace std;
 typedef long long ll;
 
+mt19937 mrand(random_device{}());
+int rnd(int x) { return mrand() % x; }
+
 class Solution {
- public:
+public:
   void solve() {
     int n;
-    cin >> n;
+    scanf("%d", &n);
   }
 };
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   int t;
-  cin >> t;
+  scanf("%d", &t);
   while (t--) {
     Solution solution = Solution();
     solution.solve();
