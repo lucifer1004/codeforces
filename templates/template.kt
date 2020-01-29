b697211e05e5e0642e1b8864bb38be79443ae656
Gabriel Wu <qqbbnease1004@126.com>
2020-01-29 14:03:20 +0800

    template: update Kotlin

--- templates/template.kt
+++ templates/template.kt
@@ -1,7 +1,41 @@
-import java.util.Scanner
- 
+import java.math.BigInteger
+
+fun readInt(): Int {
+    return readLine()!!.toInt()
+}
+
+fun readLong(): Long {
+    return readLine()!!.toLong()
+}
+
+fun readBigInt(): BigInteger {
+    return readLine()!!.toBigInteger()
+}
+
+fun readInts(): List<Int> {
+    return readLine()!!.split(" ").map(String::toInt)
+}
+
+fun readLongs(): List<Long> {
+    return readLine()!!.split(" ").map(String::toLong)
+}
+
+fun readBigInts(): List<BigInteger> {
+    return readLine()!!.split(" ").map(String::toBigInteger)
+}
+
+class Solution(private val n: Int, private val a: List<Int>) {
+    fun solve() {
+        
+    }
+}
+
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val n = scanner.nextInt()
-    println(n)
+    val t = readInt()
+    for (i in 1 until t + 1) {
+        val n = readInt()
+        val a = readInts()
+        val solution = Solution(n, a)
+        solution.solve()
+    }
 }
