f259d2f62da028fd8a32cf6bc3268498c3a842d6
Gabriel Wu <qqbbnease1004@126.com>
2020-04-22 08:48:55 +0800

    templates: update Python template

--- templates/template.py
+++ templates/template.py
@@ -1,4 +1,12 @@
-t = int(input())
+def read_int():
+    return int(input())
+
+
+def read_ints():
+    return map(int, input().split(' '))
+
+
+t = read_int()
 for case_num in range(t):
-    n = int(input())
-    a = map(int, input().split(' '))
+    n = read_int()
+    a = list(read_ints())
