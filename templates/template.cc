edbd78537939d4c3cce70149eca24686c3bdbac3
Gabriel Wu <qqbbnease1004@126.com>
2020-06-22 07:36:21 +0800

    template: update C++ template (use fast read)

--- templates/template.cc
+++ templates/template.cc
@@ -29,11 +29,23 @@ typedef long long ll;
 mt19937 mrand(random_device{}());
 int rnd(int x) { return mrand() % x; }
 
+template <typename T> void read(T &x) {
+  x = 0;
+  char c = getchar();
+  T sig = 1;
+  for (; !isdigit(c); c = getchar())
+    if (c == '-')
+      sig = -1;
+  for (; isdigit(c); c = getchar())
+    x = (x << 3) + (x << 1) + c - '0';
+  x *= sig;
+}
+
 class Solution {
 public:
   void solve() {
     int n;
-    scanf("%d", &n);
+    read(n);
   }
 };
 
@@ -41,7 +53,7 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int t;
-  scanf("%d", &t);
+  read(t);
   while (t--) {
     Solution solution = Solution();
     solution.solve();
