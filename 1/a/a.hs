04dde4d99bee7615eb23ea768051b85e95b7e0f4
Gabriel Wu <qqbbnease1004@126.com>
2020-07-31 15:21:42 +0800

    1: A (hs: use Integer instead of GHC.Int.Int64)

--- 1/a/a.hs
+++ 1/a/a.hs
@@ -1,12 +1,10 @@
-import GHC.Int
-
 convert :: Read a => String -> [a]
 convert = map read . words
 
 main :: IO ()
 main = do
   line <- getLine
-  let [n, m, a] = convert line :: [GHC.Int.Int64]
+  let [n, m, a] = convert line :: [Integer]
   let x = (n - 1) `div` a + 1
   let y = (m - 1) `div` a + 1
   print (x * y)  
