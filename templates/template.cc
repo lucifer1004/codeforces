f244a57ae4375b012eca0501c8492c3aef369523
Gabriel Wu <qqbbnease1004@126.com>
2020-08-21 11:37:55 +0800

    update: C++ template

--- templates/template.cc
+++ templates/template.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <bitset>
+#include <chrono>
 #include <climits>
 #include <cmath>
 #include <complex>
@@ -7,7 +8,9 @@
 #include <cstring>
 #include <ctime>
 #include <deque>
+#include <ext/pb_ds/assoc_container.hpp>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <list>
@@ -22,12 +25,38 @@
 #include <vector>
 #define INF 0x3f3f3f3f
 #define MOD 1000000007
+#define umap gp_hash_table<int, int, custom_hash>
 
 using namespace std;
+using namespace __gnu_pbds;
 typedef long long ll;
 
 mt19937 mrand(random_device{}());
 int rnd(int x) { return mrand() % x; }
+
+struct custom_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()(const string &s) const {
+    static const uint64_t FIXED_RANDOM =
+        chrono::steady_clock::now().time_since_epoch().count();
+    uint64_t hash = 0;
+    for (char c : s)
+      hash = (hash * 10) - '0';
+    return splitmix64(hash + FIXED_RANDOM);
+  }
+
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM =
+        chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
 
 template <typename T> void read(T &x) {
   x = 0;
