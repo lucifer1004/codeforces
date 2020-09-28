e1ebd4aa32f3f42e4b841ca6f1d8919191a44b95
Gabriel Wu <qqbbnease1004@126.com>
2020-09-28 19:49:09 +0800

    1426: update

--- 1426/f/f.cc
+++ 1426/f/f.cc
@@ -10,25 +10,24 @@ int main() {
   string s;
   cin >> s;
   ll a = 0, ab = 0, abc = 0, t = 1;
-  int q = 0;
   for (char c : s) {
     switch (c) {
-      case 'a':
-        a = (a + t) % MOD;
-        break;
-      case 'b':
-        ab = (ab + a) % MOD;
-        break;
-      case 'c':
-        abc = (abc + ab) % MOD;
-        break;
-      default:
-        ll na = a, nab = ab, nabc = abc;
-        na = (a * 3 + t) % MOD;
-        nab = (ab * 3 + a) % MOD;
-        nabc = (abc * 3 + ab) % MOD;
-        t = t * 3 % MOD;
-        a = na, ab = nab, abc = nabc;
+    case 'a':
+      a = (a + t) % MOD;
+      break;
+    case 'b':
+      ab = (ab + a) % MOD;
+      break;
+    case 'c':
+      abc = (abc + ab) % MOD;
+      break;
+    default:
+      ll na = a, nab = ab, nabc = abc;
+      na = (a * 3 + t) % MOD;
+      nab = (ab * 3 + a) % MOD;
+      nabc = (abc * 3 + ab) % MOD;
+      t = t * 3 % MOD;
+      a = na, ab = nab, abc = nabc;
     }
   }
   cout << abc;
